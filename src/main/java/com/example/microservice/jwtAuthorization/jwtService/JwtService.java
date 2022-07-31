ed2c5030a93e40cc42badc025355c8ea408f41c4
Vinay-dev1345 <108272263+Vinay-dev1345@users.noreply.github.com>
2022-07-31 23:51:02 +0530

    Created functionality to handle existing users

--- src/main/java/com/example/microservice/jwtAuthorization/jwtService/JwtService.java
+++ src/main/java/com/example/microservice/jwtAuthorization/jwtService/JwtService.java
@@ -3,6 +3,7 @@ package com.example.microservice.jwtAuthorization.jwtService;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
@@ -34,32 +35,67 @@ public class JwtService {
 		return token;
 	}
 	
+	public Boolean isExistingUser(String userName) {
+		List<String> existingUser = userRepository.checkIsUserExistingOrNot(userName);
+		if(existingUser.isEmpty()) {
+			return false;
+		}else {
+			return true;
+		}
+	}
+	
 	public Map<String, Object> createOrUpdateUser(JsonObject jso) {
 		Map<String, Object> response = new HashMap<String , Object>();
-		try {
-			User user =new User();
-			Date date = new Date();
-			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-			Random ran = new Random();
-			
-			String id = Long.toString(ran.nextLong(10000000 , 99999999));
-			String name = jso.getString("userName");
-			String currentDateTime = sdf.format(date);
-			
-			user.setUser(name);
-			user.setCreatedTime(currentDateTime);
-			user.setModifiedTime(currentDateTime);
-			user.setId(id);
-			
-			userRepository.save(user);
-			String generatedToken = createToken(name);
-			
-			response.put("errors", false);
-			response.put("accessToken", generatedToken);
-			
-		}catch(Exception e) {
-			System.out.println(e.toString());
+		if(!isExistingUser(jso.getString("userName"))) {
+			try {
+				User user =new User();
+				Date date = new Date();
+				SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+				Random ran = new Random();
+				
+				String id = Long.toString(ran.nextLong(10000000 , 99999999));
+				String name = jso.getString("userName");
+				String currentDateTime = sdf.format(date);
+				
+				user.setUser(name);
+				user.setCreatedTime(currentDateTime);
+				user.setModifiedTime(currentDateTime);
+				user.setId(id);
+				
+				userRepository.save(user);
+				String generatedToken = createToken(name);
+				
+				response.put("errors", false);
+				response.put("accessToken", generatedToken);
+				
+			}catch(Exception e) {
+				System.out.println(e.toString());
+			}
 		}
+		else {
+			try {
+				SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+				Date date = new Date();
+				
+				String currentDateTime = sdf.format(date);
+				String userName = jso.getString("userName");
+				
+				int dbResult = userRepository.updateModifyTimeForUser(currentDateTime, userName);
+				if(dbResult > 0) {
+					String generatedToken = createToken(userName);
+					response.put("errors", false);
+					response.put("accessToken", generatedToken);
+				}
+				else {
+					response.put("errors", true);
+				}
+				
+			}catch(Exception e){
+				System.out.println(e.toString());
+			}
+
+		}
+
 		
 		return response;
 	}
--- src/main/java/com/example/microservice/jwtAuthorization/jwtUserRepository/UserRepository.java
+++ src/main/java/com/example/microservice/jwtAuthorization/jwtUserRepository/UserRepository.java
@@ -1,6 +1,10 @@
 package com.example.microservice.jwtAuthorization.jwtUserRepository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,5 +13,11 @@ import com.example.microservice.jwtAuthorization.jwtUserEntity.User;
 @Transactional
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
-
+	
+	@Query(value="SELECT id FROM userdata WHERE user = ?1", nativeQuery = true)
+	List<String> checkIsUserExistingOrNot(String name);
+	
+	@Modifying(clearAutomatically=true)
+	@Query(value="UPDATE userdata SET modified_time = ?1 WHERE user = ?2", nativeQuery=true)
+	int updateModifyTimeForUser(String dateModified , String name);
 }
