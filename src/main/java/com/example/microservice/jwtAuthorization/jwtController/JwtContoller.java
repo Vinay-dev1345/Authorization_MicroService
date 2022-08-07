64c99e5812eeb5dd79b9d24a8afd704271a9fca3
Vinay-dev1345 <108272263+Vinay-dev1345@users.noreply.github.com>
2022-08-07 14:16:21 +0530

    Added cross origin reference for localhost:4200

--- src/main/java/com/example/microservice/jwtAuthorization/jwtController/JwtContoller.java
+++ src/main/java/com/example/microservice/jwtAuthorization/jwtController/JwtContoller.java
@@ -11,6 +11,7 @@ import javax.json.JsonReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,6 +28,7 @@ public class JwtContoller {
 	@Autowired
 	JwtService jwtService;
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PostMapping("/user")
 	public ResponseEntity<?> createUser(@RequestBody String user){
 		Map<String , Object> responseBody = new HashMap<String , Object>();
